eb31df1d84ea5222d276169b821e72e0ca08a5ff
M187 <dony66@gmail.com>
2016-04-14 13:26:49 +0200

    Quad-tree fix... to be continued. Reworked nodes assignment.

--- app/src/main/java/com/miso/thegame/gameMechanics/Quadtree.java
+++ app/src/main/java/com/miso/thegame/gameMechanics/Quadtree.java
@@ -10,21 +10,29 @@ import java.util.List;
 
 /**
  * Created by michal.hornak on 04.12.2015.
- *
+ * <p/>
  * Use this class to split GameObjects into groups that are near on a map.
  * Then perform check for collision on a relevant group only.
- *
+ * <p/>
  * This should speed up iterating through arrays.
  */
 public class Quadtree {
 
-    private int MAX_OBJECTS = 10;
-    private int MAX_LEVELS = 4;
+    private final int MAX_OBJECTS = 10;
+    private final int MAX_LEVELS = 5;
 
     private int level;
     private ArrayList<GameObject> objects;
     private Rect bounds;
     private Quadtree[] nodes;
+
+    private class PositionIndexer {
+
+        boolean addToTopSector = false;
+        boolean addToBottomSector = false;
+        boolean addToLeftSector = false;
+        boolean addToRightSector = false;
+    }
 
     /**
      * Constructor
@@ -54,8 +62,8 @@ public class Quadtree {
      * Splits the node into 4 subnodes
      */
     private void split() {
-        int subWidth = (int) (MapManager.getWorldWidth() / (2 * level));
-        int subHeight = (int) (MapManager.getWorldHeight() / (2 * level));
+        int subWidth = (int) (MapManager.getWorldWidth() / (Math.pow(2, level)));
+        int subHeight = (int) (MapManager.getWorldHeight() / (Math.pow(2, level)));
         int x = bounds.left;
         int y = bounds.top;
 
@@ -66,124 +74,124 @@ public class Quadtree {
     }
 
     /**
-     * Determine which node the object belongs to. -1 means
-     * object cannot completely fit within a child node and is part
-     * of the parent node
+     * Determine to which child node an object belongs to.
+     *
+     * 2 ways of calculating this.
+     * 1. nonMovableObject has its x,y coordinates as a top left.
+     * 2. movableObject has its x,y coordinates as a middle position.
      */
-    private int getIndex(GameObject gO) {
-        switch (gO.getCollisionObjectType()){
+    private PositionIndexer getIndex(GameObject gameObject) {
+        switch (gameObject.getCollisionObjectType()) {
             case Obstacle:
-                return getObstacleIndex(gO);
+                return getNonMovableObjectIndex(gameObject);
             default:
-                return getMovableObjectIndex(gO);
+                return getMovableObjectIndex(gameObject);
         }
     }
 
-    private int getObstacleIndex(GameObject gO){
-        int index = -1;
-        double verticalMidpoint = bounds.left + (MapManager.getWorldWidth() / (2 * (1 + this.level)));
-        double horizontalMidpoint = bounds.top + (MapManager.getWorldHeight() / (2 * (1 + this.level)));
+    private PositionIndexer getNonMovableObjectIndex(GameObject gameObject) {
 
-        // Object can completely fit within the top quadrants
-        boolean leftQuadrant = (gO.x + gO.getImage().getWidth() <= horizontalMidpoint);
-        // Object can completely fit within the bottom quadrants
-        boolean rightQuadrant = (gO.x >= horizontalMidpoint);
+        double verticalMidpoint = bounds.left + (MapManager.getWorldWidth() / (Math.pow(2, level)));
+        double horizontalMidpoint = bounds.top + (MapManager.getWorldHeight() / (Math.pow(2, level)));
 
-        // Object can completely fit within the left quadrants
-        if (gO.y + gO.getImage().getHeight() <= verticalMidpoint) {
-            if (leftQuadrant) {
-                index = 0;
-            } else if (rightQuadrant) {
-                index = 2;
-            }
-        }
-        // Object can completely fit within the right quadrants
-        else if (gO.y >= verticalMidpoint) {
-            if (leftQuadrant) {
-                index = 1;
-            } else if (rightQuadrant) {
-                index = 3;
-            }
-        }
+        PositionIndexer position = new PositionIndexer();
+        position.addToLeftSector = ((gameObject.x + gameObject.getImage().getWidth() <= horizontalMidpoint) ? true : false);
+        position.addToRightSector = ((gameObject.x >= horizontalMidpoint) ? true : false);
+        position.addToTopSector = ((gameObject.y + gameObject.getImage().getHeight() <= verticalMidpoint) ? true : false);
+        position.addToBottomSector = ((gameObject.y >= verticalMidpoint) ? true : false);
 
-        return index;
+        return position;
     }
 
-    private int getMovableObjectIndex(GameObject gO){
-        int index = -1;
-        double verticalMidpoint = bounds.left + (MapManager.getWorldWidth() / 2 * this.level);
-        double horizontalMidpoint = bounds.top + (MapManager.getWorldHeight() / 2 * this.level);
+    private PositionIndexer getMovableObjectIndex(GameObject gameObject) {
 
-        // Object can completely fit within the top quadrants
-        boolean leftQuadrant = (gO.x + gO.getImage().getWidth() / 2 <= horizontalMidpoint);
-        // Object can completely fit within the bottom quadrants
-        boolean rightQuadrant = (gO.x - gO.getImage().getWidth() / 2 >= horizontalMidpoint);
+        double verticalMidpoint = bounds.left + (MapManager.getWorldWidth() / (Math.pow(2, level)));
+        double horizontalMidpoint = bounds.top + (MapManager.getWorldHeight() / (Math.pow(2, level)));
 
-        // Object can completely fit within the left quadrants
-        if (gO.y + gO.getImage().getHeight() / 2 <= verticalMidpoint) {
-            if (leftQuadrant) {
-                index = 0;
-            } else if (rightQuadrant) {
-                index = 2;
-            }
-        }
-        // Object can completely fit within the right quadrants
-        else if (gO.y - gO.getImage().getHeight() / 2 >= verticalMidpoint) {
-            if (leftQuadrant) {
-                index = 1;
-            } else if (rightQuadrant) {
-                index = 3;
-            }
-        }
+        PositionIndexer position = new PositionIndexer();
+        position.addToLeftSector = ((gameObject.x + gameObject.getImage().getWidth() / 2 <= horizontalMidpoint) ? true : false);
+        position.addToRightSector = ((gameObject.x - gameObject.getImage().getWidth() / 2 >= horizontalMidpoint) ? true : false);
+        position.addToTopSector = ((gameObject.y + gameObject.getImage().getHeight() / 2 <= verticalMidpoint) ? true : false);
+        position.addToBottomSector = ((gameObject.y - gameObject.getImage().getHeight() / 2 >= verticalMidpoint) ? true : false);
 
-        return index;
+        return position;
     }
 
-    private enum verticalPosition{left, m, right}
-    private enum horizontalPosition{top, m, bottom}
+    /**
+     * Add gameObject to relevant child node.
+     *
+     * @param gameObject to be added.
+     * @param positionIndexer data-holder with informations to which nodes object should be added.
+     */
+    private void addToRelevantChildNodes(GameObject gameObject, PositionIndexer positionIndexer) {
+        try {
+            if (positionIndexer.addToTopSector) {
+                if (positionIndexer.addToLeftSector) {
+                    this.nodes[0].insert(gameObject);
+                }
+                if (positionIndexer.addToRightSector) {
+                    this.nodes[2].insert(gameObject);
+                }
+            }
+            if (positionIndexer.addToBottomSector) {
+                if (positionIndexer.addToLeftSector) {
+                    this.nodes[1].insert(gameObject);
+                }
+                if (positionIndexer.addToRightSector) {
+                    this.nodes[3].insert(gameObject);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("No child nodes! Adding to this node.");
+            this.objects.add(gameObject);
+        }
+    }
 
     /**
      * Insert the object into the quadtree. If the node
      * exceeds the capacity, it will split and add all
      * objects to their corresponding nodes.
      */
-    public void insert(GameObject pRect) {
+    public void insert(GameObject gameObject) {
         if (nodes[0] != null) {
-            int index = getIndex(pRect);
+            PositionIndexer positionIndexer = getIndex(gameObject);
+            addToRelevantChildNodes(gameObject, positionIndexer);
+        } else {
+            this.objects.add(gameObject);
 
-            if (index != -1) {
-                nodes[index].insert(pRect);
-
-                return;
-            }
-        }
-
-        objects.add(pRect);
-
-        if (objects.size() > MAX_OBJECTS && level < MAX_LEVELS) {
-            if (nodes[0] == null) {
-                split();
-            }
-
-            int i = 0;
-            while (i < objects.size()) {
-                int index = getIndex(objects.get(i));
-                if (index != -1) {
-                    nodes[index].insert(objects.remove(i));
-                } else {
-                    i++;
+            if (objects.size() > MAX_OBJECTS && level < MAX_LEVELS) {
+                this.split();
+                PositionIndexer positionIndexer = getIndex(gameObject);
+                while (0 < objects.size()) {
+                    addToRelevantChildNodes(objects.get(0), positionIndexer);
+                    objects.remove(0);
                 }
             }
         }
-    }
+     }
 
     /**
      * Return all objects that could collide with the given object
      */
-    public List retrieve(List returnObjects, GameObject pRect) {
-        int index = getIndex(pRect);
-        if (index != -1 && nodes[0] != null) {
-            nodes[index].retrieve(returnObjects, pRect);
+    public List retrieve(List returnObjects, GameObject gameObject) {
+        PositionIndexer positionIndexer = getIndex(gameObject);
+        if (this.nodes[0] != null) {
+            if (positionIndexer.addToTopSector) {
+                if (positionIndexer.addToLeftSector){
+                    this.nodes[0].retrieve(returnObjects, gameObject);
+                }
+                if (positionIndexer.addToRightSector){
+                    this.nodes[2].retrieve(returnObjects, gameObject);
+                }
+            }
+            if (positionIndexer.addToBottomSector) {
+                if (positionIndexer.addToLeftSector){
+                    this.nodes[1].retrieve(returnObjects, gameObject);
+                }
+                if (positionIndexer.addToRightSector){
+                    this.nodes[3].retrieve(returnObjects, gameObject);
+                }
+            }
         }
 
         returnObjects.addAll(objects);
