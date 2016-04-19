cc060e029c4e0e694e474853739bea99ad140542
M187 <dony66@gmail.com>
2016-04-19 13:33:08 +0200

    Not pushed with previous commit :/

---
+++ app/src/main/java/com/miso/thegame/gameMechanics/display/Animations/FlyingSaucer.java
@@ -0,0 +1,33 @@
+package com.miso.thegame.gameMechanics.display.Animations;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+
+import com.miso.thegame.R;
+
+/**
+ * Created by michal.hornak on 19.04.2016.
+ */
+public class FlyingSaucer extends StaticAnimation{
+
+    public FlyingSaucer(Point position , Resources res){
+        Bitmap spritesheet = BitmapFactory.decodeResource(res, R.drawable.gameobjective_spritee);
+        int height = 70;
+        int width = 70;
+        int numFrames = 8;
+
+        this.x = position.x;
+        this.y = position.y;
+
+        Bitmap[] image = new Bitmap[numFrames];
+
+        for(int i = 0; i< numFrames; i++)
+        {
+            image[i] = Bitmap.createBitmap(spritesheet, i*width, 0, width, height);
+        }
+        this.setFrames(image);
+        this.setDelay(25 * 1000000);
+    }
+}
