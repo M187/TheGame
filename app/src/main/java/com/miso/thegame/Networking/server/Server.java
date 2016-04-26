3c5a4dcfdb506f2031bb31f6a0bdb3089011475e
M187 <dony66@gmail.com>
2016-04-26 13:17:21 +0200

    Removed SyncQueue

--- app/src/main/java/com/miso/thegame/Networking/server/Server.java
+++ app/src/main/java/com/miso/thegame/Networking/server/Server.java
@@ -1,13 +1,14 @@
 package com.miso.thegame.Networking.server;
 
 import com.miso.thegame.Networking.MessageProcessor;
+import com.miso.thegame.Networking.transmitionData.TransmissionMessage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.SynchronousQueue;
+import java.util.List;
 
 /**
  * Created by michal.hornak on 20.04.2016.
@@ -17,11 +18,11 @@ import java.util.concurrent.SynchronousQueue;
 public class Server implements Runnable{
 
     ServerSocket myService;
-    SynchronousQueue messageHolder;
+    private volatile List<TransmissionMessage>  messageHolder;
     MessageProcessor messageProcessor = new MessageProcessor();
     private volatile boolean running = true;
 
-    public Server(int port, SynchronousQueue messageHolder) {
+    public Server(int port, List<TransmissionMessage> messageHolder) {
         this.messageHolder = messageHolder;
         try {
             this.myService = new ServerSocket(port);
@@ -34,24 +35,22 @@ public class Server implements Runnable{
         this.running = false;
     }
 
-    public void run(){
-        this.listen();
-    }
-
     /**
      * Method to listen for incoming messages and connections. Should be used in its own thread.
      */
-    private void listen() {
-        String receivedMessage;
-
+    public void run() {
         while (running) {
+            String receivedMessage;
             try {
                 Socket connectionSocket = this.myService.accept();
                 BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
                 receivedMessage = inFromClient.readLine();
+                inFromClient.close();
                 this.messageHolder.add(messageProcessor.processIncomingMessage(receivedMessage, connectionSocket.getRemoteSocketAddress()));
 
                 System.out.println(" --- > Received: " + receivedMessage);
+
+
             } catch (IOException e){
                 e.printStackTrace();
             }
