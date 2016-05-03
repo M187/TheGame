0fe28ddd486ebae1e7a698ea73bc88880807bae0
M187 <dony66@gmail.com>
2016-05-03 21:55:10 +0200

    Created skelet for processing of arriving messages.

--- app/src/main/java/com/miso/thegame/gameMechanics/multiplayer/NetworkGameStateUpdater.java
+++ app/src/main/java/com/miso/thegame/gameMechanics/multiplayer/NetworkGameStateUpdater.java
@@ -1,8 +1,9 @@
 package com.miso.thegame.gameMechanics.multiplayer;
 
-import android.view.View;
-
 import com.miso.thegame.Networking.transmitionData.TransmissionMessage;
+import com.miso.thegame.Networking.transmitionData.ingameMessages.PlayerShootProjectile;
+import com.miso.thegame.gameMechanics.collisionHandlers.CollisionObjectType;
+import com.miso.thegame.gameViews.GameView2;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -13,25 +14,56 @@ import java.util.Iterator;
 public class NetworkGameStateUpdater {
 
     private volatile ArrayList<TransmissionMessage> recievedUpdates;
-    private View multiplayerViewInstance;
+    private GameView2 multiplayerViewInstance;
 
-    public NetworkGameStateUpdater(ArrayList<TransmissionMessage> recievedUpdates, View multiplayerViewInstance){
+    public NetworkGameStateUpdater(ArrayList<TransmissionMessage> recievedUpdates, GameView2 multiplayerViewInstance) {
         this.recievedUpdates = recievedUpdates;
         this.multiplayerViewInstance = multiplayerViewInstance;
     }
 
-    public void processRecievedMessages(){
+    public void processRecievedMessages() {
 
         Iterator messageIterator = this.recievedUpdates.iterator();
-        while (messageIterator.hasNext()){
-            processEvent((TransmissionMessage)messageIterator.next());
+        while (messageIterator.hasNext()) {
+            processEvent((TransmissionMessage) messageIterator.next());
             messageIterator.remove();
         }
     }
 
-    private void processEvent(TransmissionMessage transmissionMessage){
-        switch (transmissionMessage.getTransmissionType()){
+    private void processEvent(TransmissionMessage transmissionMessage) {
+        switch (transmissionMessage.getTransmissionType()) {
+
+            //Player position
             case "10":
+                //TODO: update relevant player.
+                break;
+
+            //Shooting
+            case "20":
+                this.multiplayerViewInstance.spellManager.spellCreator
+                        .addFireball(
+                                ((PlayerShootProjectile) transmissionMessage).getFromPosition().x,
+                                ((PlayerShootProjectile) transmissionMessage).getFromPosition().y,
+                                ((PlayerShootProjectile) transmissionMessage).getDelta().x,
+                                ((PlayerShootProjectile) transmissionMessage).getDelta().y,
+                                CollisionObjectType.SpellEnemy
+                        );
+                break;
+
+            //Hit by spell
+            case "30":
+                //TODO: remove relevant projectile and add animation.
+                break;
+
+            //Player defeated
+            case "40":
+                //TODO: remove relevant player amd add animation.
+                break;
+
+            //Sync mess
+            case "50":
+                //TODO: mark player as ready for next frame.
+                break;
         }
     }
 }
