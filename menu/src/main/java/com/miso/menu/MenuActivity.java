236046766c139fcafba2f2aa94f4c5a5099d42b4
michal.hornak <dony66@gmail.com>
2017-02-03 09:42:59 +0100

    Breaking into 2 modules - menu and game. First shot

---
+++ menu/src/main/java/com/miso/menu/MenuActivity.java
@@ -0,0 +1,96 @@
+package com.miso.menu;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.miso.thegame.GameActivitySingleplayer;
+import com.miso.thegame.MultiplayerLobby;
+import com.miso.thegame.PlayerOptions;
+import com.miso.thegame.gameMechanics.map.levels.NewLevelActivity;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MenuActivity extends AppCompatActivity {
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    public static DisplayMetrics metrics = new DisplayMetrics();
+    public static boolean isGameOn = false;
+    private Intent gameIntent = null;
+    private MediaPlayer mMediaPlayer;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer = MediaPlayer.create(this, R.raw.intro);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setLooping(false);
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        //mMediaPlayer.start();
+    }
+
+    public void onResume(){
+        super.onResume();
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void newGameClickGround(View view) {
+        setContentView(R.layout.loading_game);
+        this.gameIntent = new Intent(this, NewLevelActivity.class);
+        this.gameIntent.putExtra("Level", "Ground");
+        this.isGameOn = true;
+        startActivity(this.gameIntent);
+    }
+
+    public void newGameClickSpace(View view) {
+        setContentView(R.layout.loading_game);
+        this.gameIntent = new Intent(this, GameActivitySingleplayer.class);
+        this.gameIntent.putExtra("Level", "Space");
+        this.isGameOn = true;
+        startActivity(this.gameIntent);
+    }
+
+    public void newMultiplayerGame(View view){
+        this.gameIntent = new Intent(this, MultiplayerLobby.class);
+        this.gameIntent.putExtra("Level", "Default");
+        startActivity(this.gameIntent);
+    }
+
+    public void resumeGameClick(View view){
+        try {
+            startActivity(this.gameIntent);
+        } catch (NullPointerException e){
+            System.out.println("Too bad - no game launched yet.");
+        }
+    }
+
+    public void onDestroy(){
+        mMediaPlayer.stop();
+        super.onDestroy();
+    }
+
+    public void playerOptionClick(View view){
+        Intent intent = new Intent(this, PlayerOptions.class);
+        startActivity(intent);
+    }
+
+    public void quitClick(View viev){
+        this.finish();
+        System.exit(0);
+    }
+}
